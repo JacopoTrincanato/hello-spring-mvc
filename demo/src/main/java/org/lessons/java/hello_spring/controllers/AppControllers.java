97a340d62468d458602d188fa7bc8b95a0d011d5
JacopoTrincanato <trincanatoj@gmail.com>
2025-03-10 15:08:28 +0100

    fix dynamic parametre bug

--- demo/src/main/java/org/lessons/java/hello_spring/controllers/AppControllers.java
+++ demo/src/main/java/org/lessons/java/hello_spring/controllers/AppControllers.java
@@ -1,6 +1,9 @@
 package org.lessons.java.hello_spring.controllers;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AppControllers {
 
     @GetMapping("/")
-    public String homepage() {
-
+    public String homepage(Model model) {
+        // aggiungo l'attributo a model
+        model.addAttribute("currentYear", LocalDate.now().getYear());
         return "homepage";
     }
 }
