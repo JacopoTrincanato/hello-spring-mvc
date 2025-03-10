37d68212672c781499b792dbb514fee4b7c1d5ef
JacopoTrincanato <trincanatoj@gmail.com>
2025-03-10 15:23:03 +0100

    create companyInfo.html file and update AppControllers.java

--- demo/src/main/java/org/lessons/java/hello_spring/controllers/AppControllers.java
+++ demo/src/main/java/org/lessons/java/hello_spring/controllers/AppControllers.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -17,5 +18,14 @@ public class AppControllers {
         // aggiungo l'attributo a model
         model.addAttribute("currentYear", LocalDate.now().getYear());
         return "homepage";
+    }
+
+    @GetMapping("/info")
+    public String companyInfo(@RequestParam(name = "name") String name, Model model) {
+        // aggiungo gli attributi a model
+        model.addAttribute("name", name);
+        model.addAttribute("phoneNumer", "+39 123 45 67 890");
+        model.addAttribute("email", name + "@gmail.com");
+        return "companyInfo";
     }
 }
